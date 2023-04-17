4dfcad605822e6f45e8919e42ee18f26234ffa46
endorpersand <daendrdragon@gmail.com>
2023-04-16 22:24:44 -0400

    drop deref

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,48 @@
 use std::cell::{OnceCell, Cell};
-use std::ops::Deref;
 
 pub struct Thunk<T, F = fn() -> T> {
     inner: OnceCell<T>,
     init: Cell<Option<F>>
+}
+
+impl<T> Thunk<T> {
+    pub fn empty() -> Self {
+        Thunk { inner: OnceCell::new(), init: Cell::new(Some(|| panic!("thunk value unknown"))) }
+    }
 }
 impl<T, F: FnOnce() -> T> Thunk<T, F> {
     pub fn new(f: F) -> Self {
         Thunk { inner: OnceCell::new(), init: Cell::new(Some(f)) }
     }
 
-    pub fn get_ref<'a>(this: &'a Thunk<T, F>) -> Thunk<&'a T, Box<dyn FnOnce() -> &'a T + 'a>> {
-        Thunk::new(Box::new(|| Thunk::force(this)))
+    pub fn as_ref<'a>(&'a self) -> Thunk<&'a T, Box<dyn FnOnce() -> &'a T + 'a>> {
+        Thunk::new(Box::new(|| Thunk::get(self)))
     }
 
-    pub fn force(this: &Thunk<T, F>) -> &T {
-        this.inner.get_or_init(|| match this.init.take() {
+    pub fn get(&self) -> &T {
+        self.inner.get_or_init(|| match self.init.take() {
             Some(f) => f(),
             None => panic!("no initializer"),
         })
     }
 
-    pub fn map<'a, U>(this: Thunk<T, F>, f: impl FnOnce(T) -> U + 'a) -> Thunk<U, Box<dyn FnOnce() -> U + 'a>> 
+    pub fn map<'a, U>(self, f: impl FnOnce(T) -> U + 'a) -> Thunk<U, Box<dyn FnOnce() -> U + 'a>> 
         where T: 'a, F: 'a
     {
-        Thunk::new(Box::new(|| f(Thunk::dethunk(this))))
+        Thunk::new(Box::new(|| f(Thunk::dethunk(self))))
     }
-    pub fn dethunk(this: Thunk<T, F>) -> T {
-        Thunk::force(&this);
-        this.inner.into_inner().expect("resolved dethunk")
+
+    pub fn set(&self, val: T) -> Result<(), T> {
+        self.inner.set(val)
+    }
+
+    pub fn dethunk(self) -> T {
+        Thunk::get(&self);
+        self.inner.into_inner().expect("resolved dethunk")
     }
 
     pub fn dethunk_all<I: IntoIterator<Item=Thunk<T, F>>>(t: I) -> impl Iterator<Item=T> {
         t.into_iter().map(Thunk::dethunk)
-    }
-}
-
-impl<T, F: FnOnce() -> T> Deref for Thunk<T, F> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        Thunk::force(self)
-    }
-}
-
-impl<U: 'static, T: std::ops::Add<U> + 'static, F: FnOnce() -> T + 'static> std::ops::Add<U> for Thunk<T, F> {
-    type Output = Thunk<T::Output, Box<dyn FnOnce() -> T::Output>>;
-
-    fn add(self, rhs: U) -> Self::Output {
-        Thunk::map(self, |t| t + rhs)
     }
 }
 
@@ -66,12 +60,33 @@ mod tests {
             println!("initialized y");
             3u32
         });
-        let y = vec![Thunk::get_ref(&x), Thunk::get_ref(&x), Thunk::get_ref(&y), Thunk::get_ref(&x)];
+        let y = vec![
+            x.as_ref().map(|t| t + 14), 
+            x.as_ref().map(u32::clone), 
+            y.as_ref().map(u32::clone), 
+            x.as_ref().map(u32::clone)
+        ];
         let z: Thunk<Vec<_>, _> = Thunk::new(|| Thunk::dethunk_all(y).collect());
 
-        let xy = Thunk::map(Thunk::get_ref(&x), |t| t + 1);
+        let xy = x.as_ref().map(|t| t + 1);
+        let _ = x.set(13);
+        println!("{:?}", xy.get());
+        println!("{:?}", z.get());
+    }
 
-        println!("{:?}", *xy);
-        println!("{:?}", *z);
+    #[test]
+    fn time_travel() {
+        let y = Thunk::<usize>::empty();
+        let m = vec![1, 2, 4, 5, 9, 7, 4, 1, 2, 329, 23, 23, 21, 123, 123, 0, 324];
+        let (m, it) = m.into_iter()
+            .fold((vec![], 0), |(mut vec, r), t| {
+                vec.push(y.as_ref());
+                (vec, r.max(t))
+            });
+        y.set(it).ok().unwrap();
+        let m: Vec<_> = Thunk::dethunk_all(m)
+            .copied()
+            .collect();
+        println!("{m:?}");
     }
 }
