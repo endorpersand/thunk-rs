3a32c6f97727f48d828d54287186ff2671a03cbb
endorpersand <daendrdragon@gmail.com>
2023-05-07 10:47:11 -0400

    add Lazy and DepThunk, drop *_all

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,60 @@
 use std::cell::{OnceCell, Cell};
 
+pub trait Lazy<T> {
+    fn resolve_ref(&self) -> &T;
+    fn resolve_mut(&mut self) -> &mut T;
+    fn resolve(self) -> T;
+}
+
+impl<T> Lazy<T> for T {
+    fn resolve_ref(&self) -> &T {
+        self
+    }
+
+    fn resolve_mut(&mut self) -> &mut T {
+        self
+    }
+
+    fn resolve(self) -> T {
+        self
+    }
+}
+
+impl<T, F: FnOnce() -> T> Lazy<T> for Thunk<T, F> {
+    fn resolve_ref(&self) -> &T {
+        self.force()
+    }
+
+    fn resolve_mut(&mut self) -> &mut T {
+        self.force_mut()
+    }
+
+    fn resolve(self) -> T {
+        self.dethunk()
+    }
+}
+impl<T, A> Lazy<T> for DepThunk<T, A> {
+    fn resolve_ref(&self) -> &T {
+        self.force()
+    }
+
+    fn resolve_mut(&mut self) -> &mut T {
+        self.force_mut()
+    }
+
+    fn resolve(self) -> T {
+        self.dethunk()
+    }
+}
+
 pub struct Thunk<T, F = fn() -> T> {
     inner: OnceCell<T>,
     init: Cell<Option<F>>
 }
 
 impl<T> Thunk<T> {
-    pub fn empty() -> Self {
-        Thunk { inner: OnceCell::new(), init: Cell::new(Some(|| panic!("thunk value unknown"))) }
+    pub fn undef() -> Self {
+        Thunk { inner: OnceCell::new(), init: Cell::new(Some(|| panic!("undef"))) }
     }
 }
 impl<T, F: FnOnce() -> T> Thunk<T, F> {
@@ -26,6 +73,11 @@ impl<T, F: FnOnce() -> T> Thunk<T, F> {
         })
     }
 
+    pub fn force_mut(&mut self) -> &mut T {
+        self.force();
+        self.inner.get_mut().unwrap()
+    }
+
     pub fn map<'a, U>(self, f: impl FnOnce(T) -> U + 'a) -> Thunk<U, Box<dyn FnOnce() -> U + 'a>> 
         where T: 'a, F: 'a
     {
@@ -42,19 +94,97 @@ impl<T, F: FnOnce() -> T> Thunk<T, F> {
         self.inner.into_inner().expect("resolved dethunk")
     }
 
-    pub fn dethunk_all<I: IntoIterator<Item=Thunk<T, F>>>(t: I) -> impl Iterator<Item=T> {
-        t.into_iter().map(Thunk::dethunk)
+    pub fn is_initialized(&self) -> bool {
+        self.inner.get().is_some()
     }
-    pub fn force_all<'a, I: IntoIterator<Item=&'a Thunk<T, F>>>(t: I) -> impl Iterator<Item=&'a T> 
-        where T: 'a, F: 'a
-    {
-        t.into_iter().map(Thunk::force)
+}
+
+pub struct DepThunk<T, A> {
+    inner: OnceCell<T>,
+    #[allow(clippy::type_complexity)]
+    init: Cell<Option<(fn(A) -> T, A)>>
+}
+
+impl<T, A> DepThunk<T, A> {
+    pub fn new(f: fn(A) -> T, a: A) -> Self {
+        Self { inner: OnceCell::new(), init: Cell::new(Some((f, a))) }
+    }
+
+    pub fn as_ref(&self) -> DepThunk<&T, &Self> {
+        DepThunk::new(|t| t.force(), self)
+    }
+
+    pub fn force(&self) -> &T {
+        self.inner.get_or_init(|| match self.init.take() {
+            Some((f, a)) => f(a),
+            None => panic!("no initializer"),
+        })
+    }
+
+    pub fn force_mut(&mut self) -> &mut T {
+        self.force();
+        self.inner.get_mut().unwrap()
+    }
+
+    #[allow(clippy::type_complexity)]
+    pub fn map<U>(self, f: fn(T) -> U) -> DepThunk<U, (fn(T) -> U, Self)> {
+        DepThunk::new(|(f, t)| f(t.dethunk()), (f, self))
+    }
+
+    pub fn set(&self, val: T) -> Result<(), T> {
+        self.init.take();
+        self.inner.set(val)
+    }
+
+    pub fn dethunk(self) -> T {
+        self.force();
+        self.inner.into_inner().expect("resolved dethunk")
+    }
+
+    pub fn is_initialized(&self) -> bool {
+        self.inner.get().is_some()
+    }
+}
+impl<T: Clone, A> DepThunk<T, A> {
+    pub fn cloned(&self) -> DepThunk<T, DepThunk<&T, &Self>> {
+        DepThunk::new(|t| t.dethunk().clone(), self.as_ref())
+    }
+}
+impl<T: Copy, A> DepThunk<T, A> {
+    pub fn copied(&self) -> DepThunk<T, DepThunk<&T, &Self>> {
+        DepThunk::new(|t| *t.dethunk(), self.as_ref())
+    }
+}
+
+impl<T: Clone> Clone for Thunk<T> {
+    fn clone(&self) -> Self {
+        Self { 
+            inner: OnceCell::clone(&self.inner),
+            init: self.init.clone()
+        }
+    }
+}
+impl<T: Clone, A: Clone> Clone for DepThunk<T, A> {
+    fn clone(&self) -> Self {
+        Self { 
+            inner: OnceCell::clone(&self.inner),
+            init: match self.init.take() {
+                Some(t) => {
+                    let tc = t.clone();
+                    self.init.replace(Some(t));
+                    Cell::new(Some(tc))
+                },
+                None => {
+                    Cell::new(None)
+                },
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Thunk;
+    use crate::{Thunk, Lazy, DepThunk};
 
     #[test]
     fn thunky() {
@@ -72,7 +202,7 @@ mod tests {
             y.as_ref().map(u32::clone), 
             x.as_ref().map(u32::clone)
         ];
-        let z: Thunk<Vec<_>, _> = Thunk::new(|| Thunk::dethunk_all(y).collect());
+        let z: Thunk<Vec<_>, _> = Thunk::new(|| y.into_iter().map(Thunk::dethunk).collect());
 
         let xy = x.as_ref().map(|t| t + 1);
         let _ = x.set(13);
@@ -81,8 +211,19 @@ mod tests {
     }
 
     #[test]
+    fn doubler() {
+        fn and<'a>(left: &'a dyn Lazy<bool>, right: &'a dyn Lazy<bool>) -> impl Lazy<bool> + 'a {
+            DepThunk::new(|(l, r)| *l.resolve_ref() && *r.resolve_ref(), (left, right))
+        }
+
+        let x: Thunk<bool> = Thunk::new(|| false);
+        let y = Thunk::undef();
+        let w: Box<dyn Lazy<bool>> = Box::new(DepThunk::new(|(x, y)| and(x, y), (&x, &y)).map(|t| !t.resolve_ref()));
+        println!("{}", (*w).resolve_ref());
+    }
+    #[test]
     fn time_travel() {
-        let y = Thunk::<usize>::empty();
+        let y = Thunk::<usize>::undef();
         let m = vec![1, 2, 4, 5, 9, 7, 4, 1, 2, 329, 23, 23, 21, 123, 123, 0, 324];
         let (m, it) = m.into_iter()
             .fold((vec![], 0), |(mut vec, r), t| {
@@ -90,7 +231,8 @@ mod tests {
                 (vec, r.max(t))
             });
         y.set(it).ok().unwrap();
-        let m: Vec<_> = Thunk::force_all(m)
+        let m: Vec<_> = m.into_iter()
+            .map(Thunk::force)
             .copied()
             .collect();
         println!("{m:?}");
