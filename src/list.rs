3cb0d4f8901eec3b08863ecfed53e8138f1c646f
endorpersand <daendrdragon@gmail.com>
2023-05-11 17:53:47 -0400

    add iter, get, len, is_empty

--- src/list.rs
+++ src/list.rs
@@ -2,6 +2,29 @@ use std::rc::Rc;
 
 use crate::{Thunk, Thunkable, ThunkAny};
 
+struct Node<T> {
+    val: Rc<ThunkAny<'static, T>>,
+    next: Option<Rc<ThunkAny<'static, Node<T>>>>
+}
+
+impl<T> Drop for Node<T> {
+    fn drop(&mut self) {
+        let mut head = self.next.take();
+        while let Some(thunk) = head {
+            if let Some(mut inner) = Rc::into_inner(thunk).and_then(Thunk::try_into_inner) {
+                head = inner.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+impl<T> Clone for Node<T> {
+    fn clone(&self) -> Self {
+        Self { val: self.val.clone(), next: self.next.clone() }
+    }
+}
+
 pub struct ThunkList<T> {
     head: Option<Node<T>>
 }
@@ -45,7 +68,7 @@ impl<T> ThunkList<T> {
         self.pushed_thunk(Thunk::of(t))
     }
 
-    pub fn split(&self) -> Option<(Rc<ThunkAny<'static, T>>, ThunkList<T>)> {
+    pub fn split_first(&self) -> Option<(Rc<ThunkAny<'static, T>>, ThunkList<T>)> {
         let Node { val, next } = self.head.as_ref()?;
         
         let head = next.as_deref()
@@ -54,6 +77,21 @@ impl<T> ThunkList<T> {
 
         Some((Rc::clone(val), ThunkList { head }))
     }
+    pub fn iter(&self) -> Iter<T> {
+        Iter(self.head.as_ref())
+    }
+    pub fn get_thunk(&self, n: usize) -> Option<&ThunkAny<'static, T>> {
+        self.iter().nth(n)
+    }
+    pub fn get(&self, n: usize) -> Option<&T> {
+        self.iter().nth(n).map(Thunk::force)
+    }
+    pub fn len(&self) -> usize {
+        self.iter().count()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
 }
 
 impl<T> Default for ThunkList<T> {
@@ -61,25 +99,15 @@ impl<T> Default for ThunkList<T> {
         Self::new()
     }
 }
-struct Node<T> {
-    val: Rc<ThunkAny<'static, T>>,
-    next: Option<Rc<ThunkAny<'static, Node<T>>>>
-}
+pub struct Iter<'a, T>(Option<&'a Node<T>>);
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a ThunkAny<'static, T>;
 
-impl<T> Drop for Node<T> {
-    fn drop(&mut self) {
-        let mut head = self.next.take();
-        while let Some(thunk) = head {
-            if let Some(mut inner) = Rc::into_inner(thunk).and_then(Thunk::try_into_inner) {
-                head = inner.next.take();
-            } else {
-                break;
-            }
-        }
-    }
-}
-impl<T> Clone for Node<T> {
-    fn clone(&self) -> Self {
-        Self { val: self.val.clone(), next: self.next.clone() }
+    fn next(&mut self) -> Option<Self::Item> {
+        let node = self.0?;
+        let val = &*node.val;
+        self.0 = node.next.as_ref().map(|t| t.force());
+        
+        Some(val)
     }
 }
