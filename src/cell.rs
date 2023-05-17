4250b474c3d96356b56c361a6d1425190bc4f43e
endorpersand <daendrdragon@gmail.com>
2023-05-16 23:37:28 -0400

    revise cell comments, fix safety bugs

--- src/cell.rs
+++ src/cell.rs
@@ -10,15 +10,27 @@ pub(crate) union CovUnsafeCell<T, const T_SIZE: usize> {
     value: ManuallyDrop<T>
 }
 
+/// Covariant version of UnsafeCell.
 impl<T> CovUnsafeCell<T, {size_of::<T>()}> {
     pub fn new(t: T) -> Self {
+        // SAFETY: T_SIZE == size_of::<T>()
         unsafe { CovUnsafeCell::new_unchecked(t) }
     }
 }
 impl<T, const T_SIZE: usize> CovUnsafeCell<T, T_SIZE> {
+    /// # Safety
+    /// 
+    /// When initialized, the `T_SIZE` parameter must at least meet `size_of::<T>()`
+    /// in order for the `*mut T` pointer obtained from `.get` to be allowed.
     pub unsafe fn new_unchecked(t: T) -> Self {
+        assert!(
+            T_SIZE >= size_of::<T>(), 
+            "cell needed {} bytes for safe access, but it only got {T_SIZE} bytes",
+            size_of::<T>()
+        );
         CovUnsafeCell { value: ManuallyDrop::new(t) }
     }
+
     /// SAFETY: Same requirements as UnsafeCell, with the additional requirement
     /// that any changes made using this pointer must maintain covariance.
     /// 
@@ -26,23 +38,26 @@ impl<T, const T_SIZE: usize> CovUnsafeCell<T, T_SIZE> {
     /// - changes are only done prior to any downcast
     /// - changes are lifetime agnostic
     pub fn get(&self) -> *mut T {
-        // SAFETY: Only way to init CovUnsafeCell is a value initialization
-        // So, UnsafeCell lends `size_of::<T>()` bytes in ptr-space
-        unsafe { self.cell.get() as *mut T }
+        // SAFETY: If the pointer exceeds the size of `T`,
+        // the UnsafeCell can access all of the space shared by `T`.
+        unsafe { &self.cell }.get() as *mut T 
     }
     pub fn get_mut(&mut self) -> &mut T {
-        // SAFETY: Only way to init CovUnsafeCell is a value initialization
+        // SAFETY: Since only way to init CovUnsafeCell is a value initialization,
+        // we can access self.value.
         unsafe {
             &mut self.value
         }
     }
     pub fn into_inner(mut self) -> T {
-        // SAFETY: Only way to init CovUnsafeCell is a value initialization.
-        // into_inner can't be used because we have a Drop impl.
-        // We consumed the whole Cell, so `self.value` is no longer accessible.
-        unsafe {
+        // SAFETY: Since only way to init CovUnsafeCell is a value initialization,
+        // we can access self.value.
+        let val = unsafe {
             ManuallyDrop::take(&mut self.value)
-        }
+        };
+        std::mem::forget(self);
+
+        val
     }
     pub fn raw_get(this: *const Self) -> *mut T {
         UnsafeCell::raw_get(this as *const UnsafeCell<[u8; T_SIZE]>) as *mut T
@@ -56,23 +71,38 @@ impl<T, const T_SIZE: usize> Drop for CovUnsafeCell<T, T_SIZE> {
         }
     }
 }
+
+/// Covariant version of OnceCell.
 pub(crate) struct CovOnceCell<T, const OT_SIZE: usize> {
     inner: CovUnsafeCell<Option<T>, OT_SIZE>
 }
 impl<T> CovOnceCell<T, {size_of::<Option<T>>()}> {
     pub fn new() -> Self {
-        CovOnceCell { inner: CovUnsafeCell::new(None) }
+        // SAFETY: OT_SIZE == size_of::<Option<T>>()
+        unsafe { CovOnceCell::new_unchecked() }
     }
 }
-impl<T, const N: usize> CovOnceCell<T, N> {
+impl<T, const OT_SIZE: usize> CovOnceCell<T, OT_SIZE> {
+    /// # Safety
+    /// 
+    /// When initialized, the `OT_SIZE` parameter must at least meet `size_of::<Option<T>>()`
+    /// in order for this cell's operations to be valid.
+    pub unsafe fn new_unchecked() -> Self {
+        CovOnceCell { inner: CovUnsafeCell::new_unchecked(None) }
+    }
     pub fn get(&self) -> Option<&T> {
         // SAFETY: See OnceCell
         unsafe { &*self.inner.get() }.as_ref()
     }
+
     pub fn get_mut(&mut self) -> Option<&mut T> {
         self.inner.get_mut().as_mut()
     }
-    /// SAFETY: This operation must maintain covariance.
+
+    /// # Safety
+    /// 
+    /// T must match the original lifetime of the cell 
+    /// in order to maintain covariance.
     pub unsafe fn set(&self, t: T) -> Result<(), T> {
         match &*self.inner.get() {
             Some(_) => Err(t),
@@ -83,11 +113,19 @@ impl<T, const N: usize> CovOnceCell<T, N> {
             },
         }
     }
-    /// SAFETY: This operation must maintain covariance.
+
+    /// # Safety
+    /// 
+    /// T must match the original lifetime of the cell 
+    /// in order to maintain covariance.
     pub unsafe fn get_or_init(&self, f: impl FnOnce() -> T) -> &T {
         self.get_or_try_init(|| Ok::<_, Infallible>(f())).unwrap_unchecked()
     }
-    /// SAFETY: This operation must maintain covariance.
+
+    /// # Safety
+    /// 
+    /// T must match the original lifetime of the cell 
+    /// in order to maintain covariance.
     pub unsafe fn get_or_try_init<E>(&self, f: impl FnOnce() -> Result<T, E>) -> Result<&T, E> {
         match self.get() {
             Some(t) => Ok(t),
@@ -101,11 +139,13 @@ impl<T, const N: usize> CovOnceCell<T, N> {
     pub fn into_inner(self) -> Option<T> {
         self.inner.into_inner()
     }
+
     pub fn take(&mut self) -> Option<T> {
         self.inner.get_mut().take()
     }
 }
 
+/// A cell which holds a value which can be taken at any time with a immutable reference.
 pub(crate) struct TakeCell<T, const OT_SIZE: usize> {
     inner: CovUnsafeCell<Option<T>, OT_SIZE>
 }
@@ -115,6 +155,10 @@ impl<T> TakeCell<T, {size_of::<Option<T>>()}> {
     }
 }
 impl<T, const OT_SIZE: usize> TakeCell<T, OT_SIZE> {
+    /// # Safety
+    /// 
+    /// When initialized, the `OT_SIZE` parameter must at least meet `size_of::<Option<T>>()`
+    /// in order for this cell's operation to be valid.
     pub unsafe fn new_unchecked(t: Option<T>) -> Self {
         TakeCell { inner: CovUnsafeCell::new_unchecked(t) }
     }
@@ -134,6 +178,7 @@ impl<T, const OT_SIZE: usize> TakeCell<T, OT_SIZE> {
     }
 }
 
+/// Covariant version of LazyCell.
 pub(crate) struct CovLazyCell<T, F, const OT_SIZE: usize, const OF_SIZE: usize> {
     inner: CovOnceCell<T, OT_SIZE>,
     init: TakeCell<F, OF_SIZE>
@@ -142,12 +187,22 @@ impl<T, F> CovLazyCell<T, F, { size_of::<Option<T>>() }, { size_of::<Option<F>>(
     where F: FnOnce() -> T
 {
     pub fn new(f: F) -> Self {
-        CovLazyCell { inner: CovOnceCell::new(), init: TakeCell::new(Some(f)) }
+        // SAFETY: OT_SIZE == size_of::<Option<T>>(), OF_SIZE == size_of::<Option<F>>()
+        unsafe { CovLazyCell::new_unchecked(f) }
     }
 }
 impl<T, F, const OT_SIZE: usize, const OF_SIZE: usize> CovLazyCell<T, F, OT_SIZE, OF_SIZE> 
     where F: FnOnce() -> T
 {
+    /// # Safety
+    /// 
+    /// When initialized, the `OT_SIZE` parameter must at least meet `size_of::<Option<T>>()`,
+    /// and `OF_SIZE` parameter must at least meet `size_of::<Option<F>>()`.
+    /// in order for this cell's operation to be valid.
+    pub unsafe fn new_unchecked(f: F) -> Self {
+        CovLazyCell { inner: CovOnceCell::new_unchecked(), init: TakeCell::new_unchecked(Some(f)) }
+    }
+
     pub fn force(&self) -> &T {
         // SAFETY: T and F were present at the creation of the cell,
         // so they must have compatible lifetimes
