f7ee876b36aa8263cbf2d90de159d76bb360127d
endorpersand <daendrdragon@gmail.com>
2023-05-27 12:31:17 -0400

    add collect_lazy util, rm uses of ThunkFn

--- src/iter.rs
+++ src/iter.rs
@@ -1,6 +1,7 @@
 use std::iter::FusedIterator;
 
-use crate::{Thunk, ThunkFn, Thunkable, transform};
+use crate::list::ThunkList;
+use crate::{Thunk, Thunkable, transform};
 
 pub trait ThunkItertools: Iterator {
     fn map_delayed<U, F>(self, f: F) -> MapThunk<Self, F>
@@ -24,20 +25,25 @@ pub trait ThunkItertools: Iterator {
     {
         Resolved(self)
     }
+    fn collect_lazy<'a, F: Thunkable + 'a>(self) -> ThunkList<'a, F::Item>
+        where Self: Sized + Iterator<Item = F> + 'a
+    {
+        ThunkList::from_iter_lazy(self.map(Thunkable::into_thunk))
+    }
 }
 impl<I: Iterator> ThunkItertools for I {}
 
 pub struct MapThunk<I, F>(I, F);
 impl<R, I: Iterator, F: FnMut(I::Item) -> R + Copy> Iterator for MapThunk<I, F> {
-    type Item = Thunk<R, transform::Map<ThunkFn<I::Item>, F>>;
+    type Item = Thunk<R, transform::Map<Thunk<I::Item, crate::transform::Known<I::Item>>, F>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|t| ThunkFn::of(t).map(self.1).into_thunk())
+        self.0.next().map(|t| Thunk::from(t).map(self.1).into_thunk())
     }
 }
 impl<R, I: DoubleEndedIterator, F: FnMut(I::Item) -> R + Copy> DoubleEndedIterator for MapThunk<I, F> {
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.0.next_back().map(|t| ThunkFn::of(t).map(self.1).into_thunk())
+        self.0.next_back().map(|t| Thunk::from(t).map(self.1).into_thunk())
     }
 }
 impl<R, I: ExactSizeIterator, F: FnMut(I::Item) -> R + Copy> ExactSizeIterator for MapThunk<I, F> {
